55e95ea89d4d87351e602e9f8e0de01b4dd1ca05
tarakc02 <tarakc02@github.com>
2016-06-08 18:26:27 -0700

    better printing

--- R/atomic.R
+++ R/atomic.R
@@ -1,6 +1,6 @@
 is_atomic <- function(lb) UseMethod("is_atomic")
 is_atomic.listbuilder <- function(lb) {
-    !get_operator(lb) %in% c("intersect", "union", "minus")
+    !any(get_operator(lb) %in% c("intersect", "union", "minus"))
 }
 
 is_flist <- function(lb) UseMethod("is_flist")
--- R/listbuilder.R
+++ R/listbuilder.R
@@ -12,3 +12,43 @@ as.listbuilder.character <- function(x, id_type) {
 
 #' @export
 print.listbuilder <- function(x) cat("Listbuilder object")
+
+print.listbuilder <- function(x) {
+    id <- get_id_type(x)
+    cat("Listbuilder object (type: ", id, ")\n", sep = "")
+    print_details(x)
+}
+
+print_details <- function(x) {
+    if (!is_atomic(x)) {
+        print_details(get_lhs(x))
+        print_details(get_rhs(x))
+        return()
+    }
+    conditions <- c(x$where, x$having)
+    operators <- vapply(conditions, expression_op,
+                        FUN.VALUE = character(1))
+    fields <- vapply(conditions, expression_field,
+                     FUN.VALUE = character(1))
+    vals <- vapply(conditions, expression_filter,
+                   FUN.VALUE = character(1))
+
+    for (i in seq_along(operators)) {
+        op <- operators[[i]]
+        field <- fields[[i]]
+        val <- vals[[i]]
+        cat(field, ": ", val, sep = "")
+    }
+}
+
+expression_op <- function(exp) {
+    as.character(as.list(exp)[[1]])
+}
+
+expression_field <- function(exp) {
+    as.character(as.list(exp)[[2]])
+}
+
+expression_filter <- function(exp) {
+    as.character(as.list(exp)[[3]])
+}
