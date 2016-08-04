c20ff81e2a062f18c099e07b4811240b527c21b1
tarakc02 <tarakc02@users.noreply.github.com>
2016-08-04 16:09:13 -0700

    use getcdw default for get_cdw dsn

--- R/get_cdw.R
+++ R/get_cdw.R
@@ -3,17 +3,13 @@
 getcdw::get_cdw
 
 #' @export
-get_cdw.listbuilder <- function(lb, dsn = "CDW2", uid = NULL, pwd = NULL,
-                                stringsAsFactors = FALSE, ...) {
+get_cdw.listbuilder <- function(lb, ...) {
     query <- to_sql(lb)
-    getcdw::get_cdw(query, dsn = dsn, uid = uid,
-                    pwd = pwd, stringsAsFactors = stringsAsFactors, ...)
+    getcdw::get_cdw(query, ...)
 }
 
 #' @export
-get_cdw.report <- function(report, dsn = "CDW2", uid = NULL, pwd = NULL,
-                           stringsAsFactors = FALSE, ...) {
+get_cdw.report <- function(report, ...) {
     query <- to_sql(report)
-    getcdw::get_cdw(query, dsn = dsn, uid = uid, pwd = pwd,
-                    stringsAsFactors = FALSE, ...)
+    getcdw::get_cdw(query, ...)
 }
