a1d9cb479938edaf5633df247c8458d05f7195ec
Tarak Shah <tarak@BERKELEY.EDU>
2017-06-25 18:18:23 -0700

    update to work with dbplyr

--- R/process_conditions.R
+++ R/process_conditions.R
@@ -1,4 +1,6 @@
 process_conditions <- function(conditions) {
     if (length(conditions) <= 0)  return(NULL)
-    partial_sub(conditions)
+    # partial_sub(conditions)
+    if (!is.list(conditions)) return(list(conditions))
+    conditions
 }
--- R/r2sql.R
+++ R/r2sql.R
@@ -1,8 +1,9 @@
 r2sql <- function(x) {
-    if (packageVersion("dplyr") > "0.4.3") {
-        outp <- dplyr::translate_sql_(x, window = FALSE)
-    } else {
-        outp <- dplyr::translate_sql_q(x, window = FALSE)
-    }
+    # if (packageVersion("dplyr") > "0.4.3") {
+    #     outp <- dplyr::translate_sql_(x, window = FALSE)
+    # } else {
+    #     outp <- dplyr::translate_sql_q(x, window = FALSE)
+    # }
+    outp <- dbplyr::translate_sql_(x, window = FALSE)
     unquote(outp)
 }
