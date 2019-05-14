fdfc33e3cbda6bbb54cc6b2fe22ba17c823fe54a
cwolfsonseeley <cwolfsonseeley@berkeley.edu>
2019-05-14 09:00:51 -0400

    fix bug in r2sql

--- R/r2sql.R
+++ R/r2sql.R
@@ -4,6 +4,6 @@ r2sql <- function(x) {
     # } else {
     #     outp <- dplyr::translate_sql_q(x, window = FALSE)
     # }
-    outp <- dbplyr::translate_sql(x, window = FALSE)
+    outp <- dbplyr::translate_sql(x, window = FALSE, con = dbplyr:: simulate_dbi())
     unquote(outp)
 }
