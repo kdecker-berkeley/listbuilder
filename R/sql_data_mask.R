52c3a02bd37f3a3d0e3348c460128a7ed43d32a0
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2022-07-21 11:32:36 -0700

    turn to string

--- R/sql_data_mask.R
+++ R/sql_data_mask.R
@@ -5,7 +5,7 @@ sql_data_mask <- function(expr, variant, con, window = FALSE,
     # Default for unknown functions
     unknown <- setdiff(dbplyr:::all_calls(expr), names(variant))
     op <- if (strict) dbplyr:::missing_op else dbplyr:::default_op
-    top_env <- dbplyr:::ceply(unknown, op, parent = rlang::empty_env(), env = rlang::get_env(expr))
+    top_env <- dbplyr:::ceply(unknown, op, parent = rlang::empty_env(), env = rlang::get_env(toString(expr)))
 
     # Known R -> SQL functions
     special_calls <- dbplyr:::copy_env(variant$scalar, parent = top_env)
