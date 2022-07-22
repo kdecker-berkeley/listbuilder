638e0c805bd38ba90045408ca6ea909c9836c0d9
Caleb Wolfson-Seeley <cwolfsonseeley@BERKELEY.EDU>
2022-07-22 09:38:39 -0700

    fix escape

--- R/translate_sql_.R
+++ R/translate_sql_.R
@@ -32,7 +32,7 @@ translate_sql_ <- function(dots, con = NULL, vars_group = NULL, vars_order = NUL
         }
         else {
             mask <- sql_data_mask(x, variant, con = con, window = window)
-            dbplyr::escape(toString(rlang::eval_tidy(x, mask)), con = con)
+            dbplyr::escape((rlang::eval_tidy(x, mask)), con = con)
         }
     })
     dbplyr::sql(unlist(pieces))
